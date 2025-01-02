8b10b822eba2f42c241e0ba30e178d0fbdedf580
armin <armynm005@gmail.com>
2025-01-02 22:45:07 +0330

    fix(get_link_rank): fix rank

--- selenium/sample.py
+++ selenium/sample.py
@@ -9,6 +9,7 @@ driver = webdriver.Chrome(service=service)
 def get_link_rank(query, domain, num_pages=10):
     base_url = "https://www.google.com/search?q=" + query
     domain_rank = 0
+    rank = 0
     recommended_queries = []
 
     for page in range(num_pages):
@@ -19,17 +20,23 @@ def get_link_rank(query, domain, num_pages=10):
             queries_elements = driver.find_elements(By.XPATH, "//div[@class='mtv5bd']")
             recommended_queries = [query.text for query in queries_elements]
 
-        search_results = driver.find_elements(By.XPATH, "//div[@class='tF2Cxc']//a[@href]")
-        for rank, result in enumerate(search_results, start=page * 10 + 1):
+        search_results = driver.find_elements(By.XPATH, "//a[@href]")
+        for result in search_results:
             href = result.get_attribute("href")
-            if domain in href:
-                domain_rank = rank
-                break
+            jsname = result.get_attribute("jsname")
+            if href and jsname and jsname == "UWckNb":
+                rank += 1
+                if domain in href:
+                    domain_rank = rank
+                    break
+        
+        if domain_rank != 0:
+            break
 
-    return rank, recommended_queries
+    return domain_rank, recommended_queries
 
 query = "Python programming"
-rank, recommended_queries = get_link_rank(query=query, domain="python.org", num_pages=1)
+rank, recommended_queries = get_link_rank(query=query, domain="codechef.com/learn/course/python", num_pages=10)
 print(rank, recommended_queries)
 
 driver.quit()
